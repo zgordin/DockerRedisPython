a338a0688976f1c86c91bbbb238f1037a0c1de80
zgordin <52931767+zgordin@users.noreply.github.com>
2019-08-26 21:18:41 +0300

    Add files via upload

---
+++ Selenium.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+WEB_SITE ="http://192.168.99.100:5000"
+
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome(executable_path="C:\\Users\\zgordin\\Documents\\chromedriver.exe")
+    driver.implicitly_wait(20)  # give up to 20 sec to find on all find_elemets or find_elements
+    driver.get(WEB_SITE)
+    print (driver.page_source.encode("utf-8"))
