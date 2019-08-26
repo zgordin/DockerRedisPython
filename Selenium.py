314ed9ed4a0cb7a69a30932566fc886dfdff09fa
zgordin <52931767+zgordin@users.noreply.github.com>
2019-08-26 22:56:04 +0300

    Add files via upload
    remove the word "world"

--- Selenium.py
+++ Selenium.py
@@ -8,4 +8,6 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(executable_path="C:\\Users\\zgordin\\Documents\\chromedriver.exe")
     driver.implicitly_wait(20)  # give up to 20 sec to find on all find_elemets or find_elements
     driver.get(WEB_SITE)
-    print (driver.page_source.encode("utf-8"))
+    originalstr = str(driver.page_source.encode("utf-8"))
+    newres = originalstr.replace("World", "")
+    print(newres)
